40a459d15746dc22d0941b1cb15061d7b21319b4
Luca Kenzo <luca.kenzo0204@gmail.com>
2024-08-08 12:46:24 -0300

    ok

---
+++ http/http/Services/PostServices.cs
@@ -0,0 +1,45 @@
+﻿using http.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace http.Services
+{
+    internal class PostServices
+    {
+        private HttpClient httpClient;
+        private List<Post> posts;
+        private JsonSerializerOptions jsonSerializerOptions;
+
+        public PostServices()
+        {
+            httpClient = new HttpClient();
+            jsonSerializerOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = true
+            };
+        }
+
+        public async Task<List<Post>> GetPostsAsync()
+        {
+            Uri uri = new Uri("https://jsonplaceholder.typicode.com/posts");
+            try
+            {
+                HttpResponseMessage response = await httpClient.GetAsync(uri);
+                if (response.IsSuccessStatusCode) { 
+                    string content = await response.Content.ReadAsStringAsync();
+                    posts = JsonSerializer.Deserialize<List<Post>>(content, jsonSerializerOptions);
+                }
+            }
+            catch
+            {
+
+            }
+            return posts;
+        }
+    }
+}
---
+++ http/http/Views/PostView.xaml.cs
@@ -0,0 +1,9 @@
+namespace http.Views;
+
+public partial class NewPage1 : ContentPage
+{
+	public NewPage1()
+	{
+		InitializeComponent();
+	}
+}
